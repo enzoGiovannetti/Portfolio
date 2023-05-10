e6a3f969b61460211e341f55ea37a4c8626158ff
Enzo Giovannetti <enzogtti@gmail.com>
2023-05-10 19:14:20 -0300

    Update downloader.py

--- Youtube downloader/downloader.py
+++ Youtube downloader/downloader.py
@@ -16,7 +16,7 @@ def remove_especial (old):
 tipo = input('Digite como você quer baixar(video, musica ou playlist): ')
 
 if tipo == 'musica':       
-    yt = YouTube(input('Insira aqui seu link: '))
+    yt = YouTube(input('Insira aqui seu link: '), use_oauth=True, allow_oauth_cache=True)
     #yt.streams.filter(only_audio=True, file_extension='mp3').first()
     #stream = yt.streams.get_audio_only()
     yt.streams.filter(file_extension='mp4')
@@ -32,28 +32,23 @@ if tipo == 'musica':
 
 elif tipo == 'video':
     yt = YouTube(input('Insira aqui seu link: '))
-    yt.streams.filter(file_extension='mp4')
-    stream = yt.streams.get_highest_resolution()
+    yt.streams.filter(progressive=True, resolution='2160p', file_extension='mp4')
+    stream = yt.streams.get_by_itag(22)
     titulo = remove_especial(yt.title)
     stream.download(filename=f"{titulo}.mp4")
 
 
 elif tipo == 'playlist':
     playlista = Playlist(input('Insira aqui seu link: '))
-    #z = zipfile.ZipFile('playlist.zip', 'w', zipfile.ZIP_DEFLATED)
-    #os.mkdir('playlist')
-    for music in playlista.videos:
-        music.streams.filter(file_extension='mp4')
-        stream = music.streams.get_lowest_resolution()
-        titulo = remove_especial(music.title)
+    for url in playlista.video_urls:
+        yt = YouTube(url,use_oauth=True, allow_oauth_cache=True)
+        stream = yt.streams.get_lowest_resolution()
+        titulo = remove_especial(yt.title)
         stream.download(filename=f"{titulo}.mp4")
         video = VideoFileClip(f'{titulo}.mp4')
         video.audio.write_audiofile(f'{titulo}.mp3')
-        sleep(5)
+        sleep(3)
         video.close()
         os.remove(f'{titulo}.mp4')
-        #os.replace({titulo}+'.mp3', f'playlist/{titulo}.mp3')
-        #z.write(f'{titulo}.mp3')
-        #os.remove(f'{titulo}.mp3')
-    #z.close()
+
  
